9632b5dd43442eb409e0131a4838ff6e2ff746e7
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-01-02 18:22:12 +0100

    Working on Meets pt2

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,3 +1,4 @@
+
 fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(checkStatus)
     .then(response => response.json())
@@ -8,7 +9,7 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
 
 function createGallery(data) {
     const peopleGallery = data.results.map(person => `
-    <div class="card">
+    <div class="card" >
         <div class="card-img-container">
             <img class="card-img" src="${person.picture.large}" alt="profile picture">
         </div>
@@ -25,10 +26,45 @@ function createGallery(data) {
 
 function checkStatus(response) {
     if (response.ok) {
-        return Promise.resolved(response);
+        return Promise.resolve(response);
     } else {
         return Promise.reject(new Error(response.statusText));
     }
 }
+
+function createModalWindow(data) {
+    const peopleModals = data.results.map(person => `
+        <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${person.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.cell}</p>
+                <p class="modal-text">${person.location.street.name} ${person.location.street.number}, ${person.location.state}. ${person.location.postcode}</p>
+                <p class="modal-text">Birthday: ${person.dob.date}</p>
+            </div>
+        </div>
+    `);
+}
+
+const gallery = document.querySelector('#gallery').children;
+console.log(gallery);
+console.log(gallery.length);
+
+// for (let i = 0; i < gallery.length; i++) {
+    gallery[0].addEventListener('click', e => {
+        if (e.target.tagName === 'DIV') {
+            console.log(e.target);
+        }
+    });
+// }
+
+
+
+
 
 
